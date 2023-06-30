dfbe04b88b1b0fd95ec29d897ad5ba18f66da93e
“ijassm” <ijass.ocean@gmail.com>
2023-06-30 11:09:46 +0530

    loop

--- task.py
+++ task.py
@@ -2,11 +2,18 @@
 # output - ocean
 #          -----
 
-user = input("enter any char: ")
+# print("""Input your option:
+#       1-Addition.
+#       2-Substraction.
+#       3-Multiplication.
+#       4-Division.
+#       5-Exit.""")
 
-print("1" * len(user))
-print(user)
-print("0" * len(user))
+# user = input("enter any char: ")
+
+# print("1" * len(user))
+# print(user)
+# print("0" * len(user))
 
 
 # year = int(input("enter your year to check your age : "))
