fbe4bce46eabde7badca8592db63e1461f78c872
“ijassm” <ijass.ocean@gmail.com>
2023-07-19 11:09:47 +0530

    updated at 19/07/2023

---
+++ dict.py
@@ -0,0 +1,44 @@
+# d = {"key1": "value1", "key2": "value2"}
+
+# person = {
+#     "name": "Adhi",
+#     "age": 12,
+#     "location": "Pondy",
+#     "skills": ["PYTHON", "JS", "JAVA"],
+# }
+
+
+# print(person["name"])
+# print(person["age"])
+# print(person["location"])
+# print(person["skills"])
+
+
+# list of persons
+
+person = [
+    {
+        "name": "Adhi",
+        "age": 12,
+        "location": "Pondy",
+        "skills": ["PYTHON", "JS", "JAVA"],
+    },
+    {
+        "name": "Logu",
+        "age": 15,
+        "location": "london",
+        "skills": ["PYTHON", "JS", "Ruby"],
+    },
+    {
+        "name": "Subi",
+        "age": 17,
+        "location": "America",
+        "skills": ["PYTHON", "R", "Ruby"],
+    },
+]
+
+
+print(person["name"])
+print(person["age"])
+print(person["location"])
+print(person["skills"])
