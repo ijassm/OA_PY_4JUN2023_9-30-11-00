7f3a9c74f6bb77283f192cd254c53e2729cd3027
“ijassm” <ijass.ocean@gmail.com>
2023-07-21 11:10:53 +0530

    methods

---
+++ list_methods2.py
@@ -0,0 +1,43 @@
+l = []
+
+l.append(2)
+l.append(3)
+l.append(1)
+l.append(4)
+l.append(5)
+l.append(3)
+
+# l.clear()
+
+# copyedList = l.copy()
+# print(copyedList)
+
+# print(l.count(3))
+
+# iterable
+
+# String
+# List
+# Tuple
+# Set
+# Dict
+
+# l.extend([6, 7])
+# l.append([9])
+
+# print(l.index(3))
+
+l.insert(0, 231)
+
+l.pop(0)
+
+# l.remove(1)
+
+# l.reverse()
+
+print(l, "before")
+
+# l.sort(reverse=True)
+l.sort()
+
+print(l, "after")
