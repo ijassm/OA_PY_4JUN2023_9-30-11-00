ba2426a140b6dc602a87624baaa0dcd7bb00f34e
“ijassm” <ijass.ocean@gmail.com>
2023-08-04 10:03:42 +0530

    inheritance - 04/08/2023

--- inheritance.py
+++ inheritance.py
@@ -7,35 +7,67 @@
 
 
 # A Python program to demonstrate inheritance
-class Person():
+# class Person():
 
-    # Constructor
+#     # Constructor
 
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+#     def __init__(self, name, id):
+#         self.name = name
+#         self.id = id
 
-    # To check if this person is an employee
+#     # To check if this person is an employee
 
-    def Display(self):
-        print(self.name, self.id)
-
-
-class Emp(Person):
-
-    def Print(self):
-        print("Emp class called")
+#     def Display(self):
+#         print(self.name, self.id)
 
 
-Emp_details = Emp("Mayank", 103)
+# class Emp(Person):
 
-# calling parent class function
-Emp_details.Display()
+#     def Print(self):
+#         print("Emp class called")
 
-# Calling child class function
-Emp_details.Print()
+
+# Emp_details = Emp("Mayank", 103)
+
+# # calling parent class function
+# Emp_details.Display()
+
+# # Calling child class function
+# Emp_details.Print()
 
 
 # Driver code
 # emp = Person("Satyam", 102)  # An Object of Person
 # emp.Display()
+
+
+# Python example to show the working of multiple
+# inheritance
+
+
+class Base1:
+    def __init__(self, str):
+        self.str1 = str
+        print("Base1")
+
+
+class Base2:
+    def __init__(self, str):
+        self.str2 = str
+        print("Base2")
+
+
+class Derived(Base1, Base2):
+    def __init__(self):
+        # Calling constructors of Base1
+        # and Base2 classes
+        Base1.__init__(self, "ocean")
+        Base2.__init__(self, "academy")
+        print("Derived")
+
+    def printStrs(self):
+        print(self.str1, self.str2)
+
+
+ob = Derived()
+ob.printStrs()
